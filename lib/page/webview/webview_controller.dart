04150219fde7f100d6f7e8b657fa0e3abf4ade9f
naufal <naufal.kotori@outlook.com>
2022-04-20 14:16:34 +0700

    Update: disable zoom

--- lib/page/webview/webview_controller.dart
+++ lib/page/webview/webview_controller.dart
@@ -14,6 +14,7 @@ class WebViewController extends GetxController {
       crossPlatform: InAppWebViewOptions(
         useShouldOverrideUrlLoading: true,
         mediaPlaybackRequiresUserGesture: false,
+        supportZoom: false
       ),
       android: AndroidInAppWebViewOptions(
         useHybridComposition: true,
